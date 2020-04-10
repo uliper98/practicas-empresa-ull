48e8a5aa66ffba8ca406a429f1bc57bb53089e4e
cexposit <cexposit@ull.edu.es>
2020-04-10 18:03:50 +0100

    new tasks

--- kaizten-blue-task/src/main/java/com/kaizten/algorithm/KaiztenBlueTaskApplication.java
+++ kaizten-blue-task/src/main/java/com/kaizten/algorithm/KaiztenBlueTaskApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.cloud.task.listener.annotation.AfterTask;
+import org.springframework.cloud.task.listener.annotation.BeforeTask;
+import org.springframework.cloud.task.listener.annotation.FailedTask;
+import org.springframework.cloud.task.repository.TaskExecution;
 import org.springframework.context.annotation.Bean;
 
 @EnableTask
@@ -15,20 +19,36 @@ public class KaiztenBlueTaskApplication {
 	}
 	
 	@Bean
-	public BlueTask RedServiceTask(){
+	public BlueTask BlueServiceTask() {
 		return new BlueTask();
 	}
 	
 	public class BlueTask implements CommandLineRunner {
 		
+		@BeforeTask
+		public void beforeTask(TaskExecution taskExecution) {
+			System.out.println("Before Executing Blue Task!");
+		}
+		
+		@AfterTask
+		public void afterTask(TaskExecution taskExecution) {
+			System.out.println("After Executing Blue Task!");
+			taskExecution.setExitMessage("Everything was fine!");
+		}
+		
+		@FailedTask
+		public void failedTask(TaskExecution taskExecution, Throwable throwable) {
+			System.out.println("Executing Blue Task has Failed!");
+		}
+		
 		@Override
-		public void run(String... strings) throws Exception {
-			System.out.println("EXECUTING BLUE TASK!");
-			if (null != strings) {
-				System.out.println("Blue parameter length: " + strings.length);
+		public void run(String... arguments) throws Exception {
+			System.out.println("Executing Blue Task:");
+			if (arguments != null) {
+				System.out.println("Blue parameter length: " + arguments.length);
 			}
-			for (int i = 0; i < strings.length; i++) {
-				System.out.println("Blue Arg " + i + " : " + strings[i]);
+			for (int i = 0; i < arguments.length; i++) {
+				System.out.println("Blue Arg " + i + " : " + arguments[i]);
 			}		
 		}
 	}
--- kaizten-red-task/src/main/java/com/kaizten/algorithm/KaiztenRedTaskApplication.java
+++ kaizten-red-task/src/main/java/com/kaizten/algorithm/KaiztenRedTaskApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.cloud.task.listener.annotation.AfterTask;
+import org.springframework.cloud.task.listener.annotation.BeforeTask;
+import org.springframework.cloud.task.listener.annotation.FailedTask;
+import org.springframework.cloud.task.repository.TaskExecution;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
 @EnableTask
+@SpringBootApplication
 public class KaiztenRedTaskApplication {
 
 	public static void main(String[] args) {
@@ -15,20 +19,36 @@ public class KaiztenRedTaskApplication {
 	}
 	
 	@Bean
-	public BlueTask RedServiceTask(){
-		return new BlueTask();
+	public RedTask RedServiceTask(){
+		return new RedTask();
 	}
 	
-	public class BlueTask implements CommandLineRunner {
+	public class RedTask implements CommandLineRunner {
+		
+		@BeforeTask
+		public void beforeTask(TaskExecution taskExecution) {
+			System.out.println("Before Executing Red Task!");
+		}
+		
+		@AfterTask
+		public void afterTask(TaskExecution taskExecution) {
+			System.out.println("After Executing Red Task!");
+			taskExecution.setExitMessage("Everything was fine!");
+		}
+		
+		@FailedTask
+		public void failedTask(TaskExecution taskExecution, Throwable throwable) {
+			System.out.println("Executing Red Task has Failed!");
+		}
 		
 		@Override
-		public void run(String...strings) throws Exception {
-			System.out.println("EXECUTING RED TASK!");
-			if (null != strings) {
-				System.out.println("Red parameter length: " + strings.length);
+		public void run(String... arguments) throws Exception {
+			System.out.println("Executing Red Task:");
+			if (arguments != null) {
+				System.out.println("Red parameter length: " + arguments.length);
 			}
-			for (int i=0;i<strings.length;++i) {
-				System.out.println("Red Arg " + i + " : "+strings[i]);
+			for (int i = 0; i < arguments.length; i++) {
+				System.out.println("Red Arg " + i + " : " + arguments[i]);
 			}		
 		}
 	}
