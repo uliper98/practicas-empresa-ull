0d83a5dc1be0169782b0c2e563a096f1e0692b73
cexposit <cexposit@ull.edu.es>
2020-04-11 17:51:04 +0100

    database connections with MariaDB

--- kaizten-blue-task/src/main/java/com/kaizten/algorithm/KaiztenBlueTaskApplication.java
+++ kaizten-blue-task/src/main/java/com/kaizten/algorithm/KaiztenBlueTaskApplication.java
@@ -1,6 +1,12 @@
 package com.kaizten.algorithm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.configuration.EnableTask;
@@ -24,10 +30,13 @@ public class KaiztenBlueTaskApplication {
 	}
 	
 	public class BlueTask implements CommandLineRunner {
+	//public class BlueTask implements ApplicationRunner {
 		
 		@BeforeTask
 		public void beforeTask(TaskExecution taskExecution) {
 			System.out.println("Before Executing Blue Task!");
+			taskExecution.setArguments(new ArrayList<>(Arrays.asList("arg1", "arg2")));
+			System.out.println(taskExecution.getArguments());
 		}
 		
 		@AfterTask
@@ -43,14 +52,41 @@ public class KaiztenBlueTaskApplication {
 		
 		@Override
 		public void run(String... arguments) throws Exception {
+			ApplicationArguments applicationArguments = new DefaultApplicationArguments(arguments);
 			System.out.println("Executing Blue Task:");
 			if (arguments != null) {
 				System.out.println("Blue parameter length: " + arguments.length);
 			}
+			//System.out.println(arguments);
 			for (int i = 0; i < arguments.length; i++) {
 				System.out.println("Blue Arg " + i + " : " + arguments[i]);
-			}		
+			}
+			//
+			System.out.println("# NonOptionArgs: " + applicationArguments.getNonOptionArgs().size());
+	        System.out.println("NonOptionArgs:");
+	        applicationArguments.getNonOptionArgs().forEach(System.out::println);
+	
+	        System.out.println("# OptionArgs: " + applicationArguments.getOptionNames().size());
+	        System.out.println("OptionArgs:");
+	        applicationArguments.getOptionNames().forEach(optionName -> {
+	            System.out.println(optionName + "=" + applicationArguments.getOptionValues(optionName));
+	        });
 		}
+	
+		/*@Override
+		public void run(ApplicationArguments args) throws Exception {
+			System.out.println("# NonOptionArgs: " + args.getNonOptionArgs().size());
+	
+	        System.out.println("NonOptionArgs:");
+	        args.getNonOptionArgs().forEach(System.out::println);
+	
+	        System.out.println("# OptionArgs: " + args.getOptionNames().size());
+	        System.out.println("OptionArgs:");
+	
+	        args.getOptionNames().forEach(optionName -> {
+	            System.out.println(optionName + "=" + args.getOptionValues(optionName));
+	        });
+		}*/
 	}
 }
 
