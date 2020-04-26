dd470c74d3748437b07c03661a992af2ce8fa2c7
cexposit <cexposit@ull.edu.es>
2020-04-26 23:45:49 +0100

    script deploy

---
+++ script-deploy.sh
@@ -0,0 +1,21 @@
+ #!/bin/bash
+clear
+
+rm -rf practicas-empresa-ull
+git clone https://github.com/kaizten/practicas-empresa-ull
+cd practicas-empresa-ull
+cd kaizten-blue-task
+mvn clean install
+echo 'Kaizten Blue Task installed'
+cd ../kaizten-red-task
+mvn clean install
+echo 'Kaizten Red Task installed'
+
+cd ..
+
+rm -rf $HOME/docker/volumes/mongodb/*
+
+export DATAFLOW_VERSION=2.4.2.RELEASE
+export SKIPPER_VERSION=2.3.2.RELEASE
+docker-compose up
+
